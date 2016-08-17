f67dc7b3099539f336a2ee95db92748ff6e3993e
chibui <chi_bui@outlook.com>
2016-08-17 15:02:26 +1000

    forth commit

--- time.rb
+++ time.rb
@@ -1,37 +1,11 @@
 # works with long main menu and one sub menu
-# sleep calculator works
-# to work on sleep feedback
+# sleep calculator - works
+# sleep feedback - works
 # to work on login
 
 require 'time'
 require 'date'
 require 'terminal-table'
-
-class User
-  def initialize(username,password)
-    @username = username
-    @password = password
-  end
-  attr_accessor :username, :password
-
-  def auth
-    puts "Please enter username"
-    username = gets.chomp.to_s
-    puts "Please enter password"
-    password = gets.chomp.to_s
-  end
-
-  def user_list # write to array
-    system "cls"
-    @user_list = Hash.new
-    rows = []
-    table = []
-    @user_list = Hash[username => password]
-    @menu.each do |username,password|
-      rows << [username,password]
-    end
-  end
-end
 
 class Alarms
   def initialize(wake_hr, wake_mins, sleep_length)
@@ -47,12 +21,10 @@ class MainMenu
   def ask
     puts "Welcome to Sleep Tuner"
     puts "Please select"
-    puts "1. Login"
-    puts "2. Wake Up Time"
-    puts "3. Sleep feedback"
-    puts "4. Sleep Time"
-    puts "5. Create Account"
-    puts "6. Exit"
+    puts "1. Wake Up Time"
+    puts "2. Sleep feedback"
+    puts "3. Sleep Time"
+    puts "4. Exit"
   end
 
   def run
@@ -63,13 +35,6 @@ class MainMenu
       menu_input = gets.chomp.to_i
 
       if menu_input == 1
-        #login.auth #capture login
-        #work on authentication method
-        cls
-        puts "logged in"
-        sleep (0.5)
-
-      elsif menu_input == 2
         #if #password matches username key in password file then grant access.
         #end
         cls
@@ -96,7 +61,10 @@ class MainMenu
         # variable that changes to adjust amount of time you sleep (includes time needed to fall asleep)
         # starts at 7.5 hours + 15 mins (25200 + 900 = 26100)
         # (typical sleep cycle consisting of 1.5 hour blocks and average time needed to fall asleep)
-        sleep_length = 27900
+        sl = 'sl.txt'
+        sleep_file = open(sl, 'r+')
+        sleep_length = sleep_file.read.to_i
+
 
         # creates time as an object
         time = Time.new
@@ -107,10 +75,11 @@ class MainMenu
         t = Time.new(time.year, time.month, next_day, "#{wake_hr}", "#{wake_mins}")
         # calculates the date and time in seconds before the above defined time
         # time - sleep_length - time to fall asleep
-        sleep_at = t - sleep_length.to_i
+        sleep_at = t - sleep_length
+        sleep_file.close
         cls
 
-        display_min = 'time_converted'.tr(',', '')
+        # display_min = 'time_converted'.tr(',', '')
         puts "Your desired wake up time is #{wake_hr}:#{wake_mins} am"
         print "You should sleep at #{sleep_at}"
         st = 'st.txt'
@@ -122,7 +91,7 @@ class MainMenu
         puts ""
         break
 
-      elsif menu_input == 3
+      elsif menu_input == 2
         cls
         puts "Sleep feedback"
 
@@ -131,7 +100,8 @@ class MainMenu
 
         sleep (0.5)
         # display sleep time
-      elsif menu_input == 4
+
+      elsif menu_input == 3
         cls
 
         st = 'st.txt'
@@ -143,18 +113,7 @@ class MainMenu
         puts ""
         break
 
-      elsif menu_input == 5
-        # login.auth # capture login
-        # userlist = []
-        # user_list.write["#{login.auth.@username},#{login.auth.@password}"]
-        # user_list.close
-        cls
-        puts "create account"
-        username = 'chi'
-        puts "#{username} created"
-        sleep (1.5)
-
-      elsif menu_input == 6
+      elsif menu_input == 4
         cls
         puts "Thanks for using Sleep Tune. Exiting"
         exit = true
@@ -178,26 +137,55 @@ class SubMenu
     puts "Please select"
     puts "1. Good"
     puts "2. Bad"
-    puts "3. Exit"
   end
 
   def run
-    exit = false
-    until exit do
+     exit = false
+     until exit do
       ask
       menu_input = gets.chomp.to_i
 
       if menu_input  == 1
         cls
         puts "No adjustment needed"
+        exit = true
 
       elsif menu_input == 2
         cls
         puts "sleep time adjusted + 10 mins" # adjust sleep length + 10 mins
 
-      elsif menu_input == 3
-        cls
-        puts "Exiting"
+        wh = 'wh.txt'
+        alarm_hr = open(wh, 'r+')
+
+        wm = 'wm.txt'
+        alarm_min = open(wm, 'r+')
+
+        time = Time.new
+        next_day = time.day + 1
+
+        # read sl file for sleep length
+        sl = 'sl.txt'
+        sleep_file = open(sl, "a+")
+        sleep_length = sleep_file.read
+
+        new_sleep_length = sleep_length.to_i + 600
+        sleep_time = Time.new(time.year, time.month, next_day,"#{alarm_hr.read}", "#{alarm_min.read}")
+        alarm_hr.close
+        alarm_min.close
+        puts new_sleep_length
+        update_time = sleep_time - new_sleep_length
+
+        sleep_file.write("\n")
+        sleep_file.rewind
+        sleep_file.write("#{new_sleep_length}")
+        sleep_file.close
+        # opens sleep time file
+        st = 'st.txt'
+        s_time = open(st, "r+" "t")
+        s_time.write("#{update_time}")
+        s_time.close
+
+        sleep (2.0)
         exit = true
 
       else
@@ -207,24 +195,10 @@ class SubMenu
   end
 end
 
-# class OpenFiles
-#   # def initialize
-#   # end
-#   def opf
-#     # opens files needed to write time variables
-#     wm = 'wm.txt'
-#     alarm_min = open(wm, 'w+')
-#
-#     wh = 'wh.txt'
-#     alarm_hr = open(wh, 'w+')
-#   end
-# end
-
 def cls
   system 'cls'
 end
 
-login = User.new("","")
 alm = Alarms.new("","","")
 
 user_file = 'uf.txt'
